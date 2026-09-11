411146ac516022c514a7f81125491dfee65dcfb0
Abhinash Rai <rainikhil3112@gmail.com>
2026-09-11 15:53:29 +0530

    complete

---
+++ lab 4/user.js
@@ -0,0 +1,9 @@
+// we use in memory database
+let user = [
+    {id:1,name:'Abhinash Rai',mob:'98345xxxxx',email:'abhinash.example@exam.com'},
+    {id:1,name:'Abhay Singh',mob:'92345xxxxx',email:'abhay.example@exam.com'},
+]
+
+let nextId = 3;
+
+export const getUsers = () => users;
