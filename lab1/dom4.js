7388c8ebfbff15b9d845187d8af8b548d38db4f9
Abhinash Rai <rainikhil3112@gmail.com>
2026-09-19 15:14:39 +0530

    complete

--- lab1/dom4.js
+++ lab1/dom4.js
@@ -1,11 +1,15 @@
 import { EventEmitter } from "events";
 
+// console.log("Form event emitter created");
 const form = new EventEmitter();
 
+// console.log("Submit listener registered");
 form.on("submit", (uname,password) => {
-    console.log("form submitted");
-    console.log(`user name: ${uname}`);
-    console.log(`user password: ${password}`);
+  console.log("form submitted");
+  console.log(`user name: ${uname}`);
+  console.log(`user password: ${password}`);
+  // console.log("Form submission handled");
 });
 
+// console.log("Submit event emitted");
 form.emit("submit", "abc@abc.com ❤️", "1122334422")
