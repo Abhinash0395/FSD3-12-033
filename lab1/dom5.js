2403c448a0151673f339cde4f21a1b99089f5d2f
Abhinash Rai <rainikhil3112@gmail.com>
2026-09-19 15:20:47 +0530

    complete

--- lab1/dom5.js
+++ lab1/dom5.js
@@ -1,28 +1,37 @@
+// console.log("EventEmitter imported");
 import { EventEmitter } from "events";
 
 class DomClass extends EventEmitter {
-    addEventListener(eventName, callback) {
-        this.on(eventName, callback);
-    }
-    removeEventListener(eventName, callback) {
-        this.off(eventName, callback);
-    }
-    dispatchEvent(eventName,eventData = {}){
-        const event = {
-            type : eventName,
-            timespam : new Date(),
-            ...eventData,
-        };
-        this.emit(eventName, event);
-    }
+  addEventListener(eventName, callback) {
+    this.on(eventName, callback);
+  }
+  removeEventListener(eventName, callback) {
+    this.off(eventName, callback);
+  }
+
+  // console.log("Dispatching event:", eventName);
+  dispatchEvent(eventName, eventData = {}) {
+    const event = {
+      type: eventName,
+      timespam: new Date(),
+      ...eventData,
+    };
+    this.emit(eventName, event);
+  }
 }
 
+// console.log("Creating button");
 const button = new DomClass();
+
+// console.log("Click handler created");
 const handleClick = (event) => {
-    console.log(`Button clicked type: ${event.type} at ${event.timespam}`);
+  console.log(`Button clicked type: ${event.type} at ${event.timespam}`);
 };
 
-button.addEventListener('click',handleClick);
+// console.log("Click listener added");
+button.addEventListener("click", handleClick);
+
+// console.log("Click event dispatched");
 button.dispatchEvent("click", {
-    target: "submitBtn",
+  target: "submitBtn",
 });
