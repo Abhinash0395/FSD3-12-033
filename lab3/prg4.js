7717d3e5f5a911c7a83f37f6173b256b67ab4218
Abhinash Rai <rainikhil3112@gmail.com>
2026-09-19 16:58:26 +0530

    complete

--- lab3/prg4.js
+++ lab3/prg4.js
@@ -3,12 +3,14 @@ import http from "http"
 const server = http.createServer((req, res) => {
     res.writeHead(200,{'content-type':'application/json'});
     console.log("Client URL:", req.url);
+
     const product = [{
         name : "Iphone",
         price : 85000,
         qty : 2,
         discount : 15,
     },
+
     {
         name : "HP Laptop",
         price : 95000,
@@ -16,7 +18,9 @@ const server = http.createServer((req, res) => {
         discount : 25,
     },
 ];
+
     res.end(JSON.stringify(product));
+    
 });
 
 server.listen(3000, () => console.log("Server is running at 3000"));
