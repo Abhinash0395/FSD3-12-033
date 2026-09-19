4745cf91f785b3970a1ae4be77806527076d4782
Abhinash Rai <rainikhil3112@gmail.com>
2026-09-19 14:35:42 +0530

    complete

--- lab1/dom2.js
+++ lab1/dom2.js
@@ -1,16 +1,22 @@
 import { EventEmitter } from "events";
 
+// console.log("Button event emitter created");
 const button = new EventEmitter();
+
+// console.log("Task 1 listener registered");
 button.on("click", () => {
   console.log("Task 1");
 });
 
+// console.log("Task 2 listener registered");
 button.on("click", () => {
   console.log("Task 2");
 });
 
+// console.log("Task 3 listener registered");
 button.on("click", () => {
   console.log("Task 3");
 });
 
+// console.log("Click event emitted");
 button.emit("click");
