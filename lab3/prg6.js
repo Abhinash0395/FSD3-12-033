c7e35b69e3ad870792c64d8619ee595a013ba240
Abhinash Rai <rainikhil3112@gmail.com>
2026-09-19 17:00:27 +0530

    complete

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -5,21 +5,29 @@ const server = http.createServer((req, res) => {
   res.setHeader("content-type", "text/html");
 
   if (req.url === "/") {
+
     res.write("<h1>Home Page</h1>");
     res.end(`
       <a href="/product">Product</a>
       <a href="/contact">Contact Us</a>
       `);
-  } else if (req.url === "/product") {
+  } 
+
+  else if (req.url === "/product") {
     const stream = createReadStream("product.html", { encoding: "utf-8" });
     stream.pipe(res);
-  } else if (req.url === "/contact") {
+  } 
+
+  else if (req.url === "/contact") {
     const stream = createReadStream("contact.html", { encoding: "utf-8" });
     stream.pipe(res);
-  } else {
+  } 
+  
+  else {
     res.statusCode = 404;
     res.end("Not found");
   }
+  
 });
 
 server.listen(3000, () => console.log("prg6 is running..."));
