c89caa207a243cdcdeb1fbba6031580e0ac3650b
Abhinash Rai <rainikhil3112@gmail.com>
2026-09-19 14:41:40 +0530

    complete

--- lab1/dom3.js
+++ lab1/dom3.js
@@ -1,14 +1,27 @@
 import {EventEmitter} from "events";
 
+// console.log("Button event emitter created");
 const button = new EventEmitter();
 
+// console.log("Click listener registered");
 button.on("click",(uname) => {
     console.log(`button clicked by ${uname}`);
 });
 
+// console.log("Raju click emitted");
 button.emit("click", "Raju");
+
+// console.log("Kaju click emitted");
 button.emit("click", "Kaju");
+
+// console.log("Anjali click emitted");
 button.emit("click", "Anjali");
+
+// console.log("Aaniya click emitted");
 button.emit("click", "Aaniya");
+
+// console.log("Rani click emitted");
 button.emit("click", "Rani");
+
+// console.log("Final click emitted");
 button.emit("click");
